0d7f9d74c5d82fa73d72a91814e2a8d9eedcc3ba
ldz <ldz@qq.com>
2022-04-20 10:03:13 +0800

    分支

--- interfacet/src/main/java/com/demo/thread/threadpool/ThreadPool01.java
+++ interfacet/src/main/java/com/demo/thread/threadpool/ThreadPool01.java
@@ -7,6 +7,7 @@ import java.util.concurrent.*;
  * @description: 通过线程池方法实现多线程
  */
 public class ThreadPool01 {
+    Integer num = 0;
     //创建一个可缓存线程池，如果线程池长度超过处理需要，可灵活回收空闲线程，若无可回收，则新建线程。
     ExecutorService executorService01 = Executors.newCachedThreadPool();
 
@@ -16,26 +17,36 @@ public class ThreadPool01 {
     //创建一个单线程化的Executor，即只创建唯一的工作者线程来执行任务，它只会用唯一的工作线程来执行任务，保证所有任务按照指定顺序(FIFO, LIFO, 优先级)执行。
     // 如果这个线程异常结束，会有另一个取代它，保证顺序执行。单工作线程最大的特点是可保证顺序地执行各个任务，并且在任意给定的时间不会有多个线程是活动的。
     ExecutorService executorService03 = Executors.newSingleThreadExecutor();
-    ThreadPoolExecutor executor = new ThreadPoolExecutor(10,30,100,
-            TimeUnit.SECONDS, (BlockingQueue<Runnable>) Executors.defaultThreadFactory(), new ThreadPoolExecutor.CallerRunsPolicy());
+    ThreadPoolExecutor executor = new ThreadPoolExecutor(100,300,100,
+            TimeUnit.SECONDS, new LinkedBlockingDeque<>(),Executors.defaultThreadFactory(), new ThreadPoolExecutor.CallerRunsPolicy());
 
     public static void main(String[] args) {
         ThreadPool01 pool01 = new ThreadPool01();
         for (int i=0;i<1000;i++)
         pool01.test();
+
+        pool01.executor.shutdown();
     }
     public void test(){
         executor.execute(new Runnable() {
             @Override
             public void run() {
-                System.out.println(Thread.currentThread().getName()+"started");
-
-                try {
-                    TimeUnit.SECONDS.sleep(10);
-                } catch (InterruptedException e) {
-                    e.printStackTrace();
+                synchronized (num){
+                    try {
+                        TimeUnit.SECONDS.sleep(1);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                    System.out.println(Thread.currentThread().getName()+"started" + ++num);
                 }
-                System.out.println(Thread.currentThread().getName()+"end");
+
+
+//                try {
+//                    TimeUnit.SECONDS.sleep(1);
+//                } catch (InterruptedException e) {
+//                    e.printStackTrace();
+//                }
+//                System.out.println(Thread.currentThread().getName()+"end");
             }
         });
     }
