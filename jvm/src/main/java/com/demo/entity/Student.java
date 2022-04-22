8cfb5765e8d0ca33e36923306441b970a57022d9
过眼烟云 <email@sample.com>
2022-04-22 11:20:53 +0800

    给Students类重写equals方法

--- jvm/src/main/java/com/demo/entity/Student.java
+++ jvm/src/main/java/com/demo/entity/Student.java
@@ -2,8 +2,24 @@ package com.demo.entity;
 
 import lombok.Data;
 
+import java.util.Objects;
+
 @Data
 public class Student {
     private Integer studentId;
     private String name;
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Student student = (Student) o;
+        return Objects.equals(studentId, student.studentId) &&
+                Objects.equals(name, student.name);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(studentId, name);
+    }
 }
